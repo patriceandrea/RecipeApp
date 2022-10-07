2541d85bcc1e9f099fb2e2a2e47e59c8dc08c0a9
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-07 17:31:49 -0400

    removes .env

--- client/src/components/Popular.js
+++ client/src/components/Popular.js
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Popular = () => {
+
+  const [popular, setPopular] = useState([]);
 
   useEffect(() => {
     getPopular();
@@ -9,8 +11,7 @@ const Popular = () => {
   const getPopular = async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
     const data = await api.json();
-    console.log(data);
-
+    setPopular(data.recipes);
   };
 
 
