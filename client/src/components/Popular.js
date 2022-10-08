0b27dcf3e9e4c11782b580483899ba155fcbd643
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-08 17:27:16 -0400

    adds index css styling

--- client/src/components/Popular.js
+++ client/src/components/Popular.js
@@ -12,22 +12,42 @@ const Popular = () => {
   }, [])
 
   const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-    const data = await api.json();
-    setPopular(data.recipes);
-  };
 
+    const check = localStorage.getItem('popular');
+
+    if (check) {
+      setPopular(JSON.parse(check))
+    } else {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+      const data = await api.json();
+
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
+
+
+  };
 
   return (
     <Wrapper>
       <h3>Popular Picks</h3>
-      <Splide>
+      <Splide
+        options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: 'free',
+          gap: '5rem'
+        }}
+      >
         {popular.map((recipe) => {
           return (
-            <SplideSlide>
+            <SplideSlide key={recipe.id}>
               <Card>
                 <p>{recipe.title}</p>
                 <img src={recipe.image} alt={recipe.title} />
+                <Gradient />
               </Card>
             </SplideSlide>
           )
@@ -39,17 +59,48 @@ const Popular = () => {
 }
 
 const Wrapper = styled.div`
-  margin: 4re, 0rem;
+  margin: 4rem, 0rem;
 `
 
 const Card = styled.div`
 min-height: 25rem;
 border-radius: 2rem;
 overflow: hidden;
+position: relative; 
 
-img{
+img {
 border-radius: 2rem; 
+position: absolute; 
+left: 0;
+width: 100%; 
+height: 100%;
+object-fit: cover; 
 }
 
+p {
+  position: absolute; 
+  z-index: 10;  
+  left: 50%; 
+  bottom: 10%;
+  transform: translate(-50%, 0%); 
+  color: white; 
+  width: 100%; 
+  text-align: center; 
+  font-weight: 600;
+  font-size: 1rem; 
+  heigth: 40%; 
+  display: flex; 
+  justify-content: center;
+  align-items: center; 
+}
+`
+
+const Gradient = styled.div`
+z-index: 3; 
+position: absolute; 
+width: 100%; 
+height: 100%; 
+background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5)); 
+ 
 `
 export default Popular; 
--- client/src/components/Veggie.js
+++ client/src/components/Veggie.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Veggie = () => {
   return (
     <div>
-      Veggie
+
     </div>
   )
 }
