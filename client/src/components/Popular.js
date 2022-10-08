32692cbfd7369f98634b49bfb6eb246fd403ff9c
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-08 16:45:49 -0400

    adds slide feature

--- client/src/components/Popular.js
+++ client/src/components/Popular.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css'
 
 const Popular = () => {
 
@@ -16,16 +19,37 @@ const Popular = () => {
 
 
   return (
-    <div>
-      {popular.map(recipe => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        )
-      })}
-    </div>
-  )
+    <Wrapper>
+      <h3>Popular Picks</h3>
+      <Splide>
+        {popular.map((recipe) => {
+          return (
+            <SplideSlide>
+              <Card>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+              </Card>
+            </SplideSlide>
+          )
+        })
+        }
+      </Splide>
+    </Wrapper>
+  );
 }
 
+const Wrapper = styled.div`
+  margin: 4re, 0rem;
+`
+
+const Card = styled.div`
+min-height: 25rem;
+border-radius: 2rem;
+overflow: hidden;
+
+img{
+border-radius: 2rem; 
+}
+
+`
 export default Popular; 
