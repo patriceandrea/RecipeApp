c3104b4fa2913f6b96c86ad9453346c82709ccde
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-11 12:20:54 -0400

    adds Link to the Category components

--- client/src/pages/Cuisine.js
+++ client/src/pages/Cuisine.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
 
 const Cuisine = () => {
   return (
--- client/src/pages/Pages.js
+++ client/src/pages/Pages.js
@@ -1,18 +1,17 @@
 import React from 'react'
 
 import Home from './Home';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Cuisine from './Cuisine';
 
 const Pages = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cuisine' element={<Cuisine />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cuisine' element={<Cuisine />} />
+      </Routes>
+
     </div>
   )
 }
